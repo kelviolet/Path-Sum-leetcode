0402aee5501cdf7b32eb06374933b8ed5ef3b3ae
Chen He <airbots@gmail.com>
2016-11-28 23:06:09 -0800

    first commit

--- PathSum.py
+++ PathSum.py
@@ -8,15 +8,13 @@ class Solution(object):
     def hasPathSum(self, root, sum):
         """
         :type root: TreeNode
-
         :type sum: int
-
         :rtype: bool
-
         """
         if root==None:
             return False
         if root.val==sum and root.left==None and root.right==None:
             return True
         return self.hasPathSum(root.left, sum-root.val)or self.hasPathSum(root.right, sum-root.val) 
+# leetcode
 # leetcode
